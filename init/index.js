e1b14647bf150469d59a3fdf81cc1427c5a9d1b3
gaurisharma <gaurisharma9105@gmail.com>
2026-01-22 08:31:24 +0530

    data initialized

--- init/index.js
+++ init/index.js
@@ -1,17 +1,19 @@
 if(process.env.NODE_ENV !== "production"){
- require('dotenv').config({ path: '../.env' });
+  require('dotenv').config({ path: '../.env' });
 }
 
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
+const User = require("../models/user.js");
+const bcrypt = require('bcrypt'); 
 
 const DB_URL = process.env.ATLASDB_URL;
-
 
 main()
   .then(() => {
     console.log("connected to DB");
+    initDB(); 
   })
   .catch((err) => {
     console.log(err);
@@ -22,10 +24,20 @@ async function main() {
 }
 
 const initDB = async () => {
+  // Dummy user 
+  let dummyUser = await User.findOne({ username: 'dummyowner' });
+  if (!dummyUser) {
+    dummyUser = new User({
+      username: 'dummyowner',
+      email: 'dummy@example.com',
+      password: bcrypt.hashSync('dummy123', 10)  
+    });
+    await dummyUser.save();
+    console.log('Dummy user created');
+  }
+
   await Listing.deleteMany({});
- initData.data= initData.data.map((obj)=>({...obj, owner : "6960d3d472d1681216f7a476"}));
+  initData.data = initData.data.map((obj) => ({ ...obj, owner: dummyUser._id }));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
-
-initDB();
